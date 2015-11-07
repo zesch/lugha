61900d61465df6776d3cdf1ee7b92a1c1249413d
LTL <sam.hamed@gmail.com>
2015-11-07 16:58:34 +0100

    Helper classes in normalization

---
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.normalization/src/main/java/de/unidue/ltl/lugha/normalization/helper/Preporcessing.java
@@ -0,0 +1,32 @@
+package de.unidue.ltl.lugha.normalization.helper;
+
+import java.util.ArrayList;
+
+public class Preporcessing {
+
+	private String removeExtraSpaces(String currentText) {
+		
+		ArrayList<String> tokens = new ArrayList<String>();
+		
+		StringBuffer word = new StringBuffer();
+
+		currentText = currentText + " ";
+		for (int i = 0; i < currentText.length(); i++) {
+
+			// if the character is not a space, add it to a word
+			if (false == Character.isWhitespace(currentText.charAt(i))) {
+				word.append(currentText.charAt(i));
+			} else {
+				if (word.length() != 0) {
+					tokens.add(word.toString());
+					word.setLength(0);
+				}
+			}
+		}
+		
+		String results = Utilities.arrayListToString(tokens);
+		
+		return results;
+	}
+
+}
