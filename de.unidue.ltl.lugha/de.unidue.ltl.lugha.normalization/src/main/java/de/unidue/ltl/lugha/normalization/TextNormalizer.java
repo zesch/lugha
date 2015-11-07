1893f25ba44710a31879a6dde8313d479e885156
LTL <sam.hamed@gmail.com>
2015-11-07 17:06:53 +0100

    all normalisation in 1 step with java documentation

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.normalization/src/main/java/de/unidue/ltl/lugha/normalization/TextNormalizer.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.normalization/src/main/java/de/unidue/ltl/lugha/normalization/TextNormalizer.java
@@ -1,15 +1,27 @@
 package de.unidue.ltl.lugha.normalization;
 
+import de.unidue.ltl.lugha.normalization.helper.Preporcessing;
+
 public class TextNormalizer {
 
 	/**
 	 * TODO description of what kinds of normalization is performed
 	 * 
-	 * @param text The input text
-	 * @return The normalized text.
+	 * @param text
+	 *   The input text:تطويـــــــــــــــــــــــــــــــــــل         هكـــــــــذا
+	 *            
+	 * @return The normalized text:تطويل هكذا
 	 */
+
 	public static String normalizeText(String text) {
 		// FIXME include several steps for normalization here
-		return "";
+
+		String withoutExtraSpaces = Preporcessing
+				.discardExtraWhiteSpaces(text);
+
+		String withoutTatweel = NormalizeArabicText
+				.normalizeTatweel(withoutExtraSpaces);
+
+		return withoutTatweel;
 	}
 }
