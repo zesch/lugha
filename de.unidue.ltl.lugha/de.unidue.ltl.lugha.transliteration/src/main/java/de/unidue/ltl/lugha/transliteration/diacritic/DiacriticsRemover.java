4aa1af48367c4e9861f573e6e485da77c6fc54a4
LTL <sam.hamed@gmail.com>
2015-11-08 21:37:22 +0100

    Java Docs

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/main/java/de/unidue/ltl/lugha/transliteration/diacritic/DiacriticsRemover.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/main/java/de/unidue/ltl/lugha/transliteration/diacritic/DiacriticsRemover.java
@@ -7,6 +7,14 @@ import de.unidue.ltl.lugha.normalization.helper.Preporcessing;
 
 public class DiacriticsRemover {
 
+	/**
+	 * Description of what kinds of DiacriticsRemover is performed
+	 * 
+	 * @param text
+	 *   The input text:(َبِّ أَعِنِّي وَلَا تُعِنْ عَلَيَّ وَانْصُرْنِي وَلَا تَنْصُرْ عَلَيَّ)
+	 *            
+	 * @return The without diacritics text:(رب أعني ولا تعن علي وانصرني ولا تنصر علي)
+	 */
 	public static String removeDiacritics(String currentText) {
 
 		
