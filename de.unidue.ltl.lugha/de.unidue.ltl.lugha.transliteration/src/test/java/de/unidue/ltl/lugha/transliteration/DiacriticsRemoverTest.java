49ba2324305a54d29773bc8e7ca774ea7a0e1443
LTL <sam.hamed@gmail.com>
2015-11-08 21:19:23 +0100

    DiacriticsRemoverTest as JUnit test for removeDiacritics method

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/test/java/de/unidue/ltl/lugha/transliteration/DiacriticsRemoverTest.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/test/java/de/unidue/ltl/lugha/transliteration/DiacriticsRemoverTest.java
@@ -1,12 +1,26 @@
 package de.unidue.ltl.lugha.transliteration;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+
+import de.unidue.ltl.lugha.transliteration.diacritic.DiacriticsRemover;
 
 public class DiacriticsRemoverTest {
 
 	@Test
 	public void diacriticsRemoverTest() {
 		
+		String withoutDiacritics = "";
+		
+//    	String sublimation = "اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ، وَعَلَى آلِ مُحَمَّدٍ";
+//    	withoutDiacritics = DiacriticsRemover.removeDiacritics(sublimation);
+//    	assertEquals("اللهم صل على محمد، وعلى آل محمد", withoutDiacritics);
+    	
+    	String duaa = "رَبِّ أَعِنِّي وَلَا تُعِنْ عَلَيَّ وَانْصُرْنِي وَلَا تَنْصُرْ عَلَيَّ";
+		withoutDiacritics = DiacriticsRemover.removeDiacritics(duaa);		
+		assertEquals("رب أعني ولا تعن علي وانصرني ولا تنصر علي", withoutDiacritics);
+
 	}
 
 }
