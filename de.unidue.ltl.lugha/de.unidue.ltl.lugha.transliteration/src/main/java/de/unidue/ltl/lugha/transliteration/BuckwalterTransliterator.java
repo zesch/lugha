a737248a330558d02e65fd92a8fc56c79e1c7f00
LTL <sam.hamed@gmail.com>
2015-11-10 20:50:54 +0100

    transilteration method implementation

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/main/java/de/unidue/ltl/lugha/transliteration/BuckwalterTransliterator.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/main/java/de/unidue/ltl/lugha/transliteration/BuckwalterTransliterator.java
@@ -1,12 +1,46 @@
 package de.unidue.ltl.lugha.transliteration;
 
+import de.unidue.ltl.lugha.normalization.TextNormalizer;
+
 public class BuckwalterTransliterator 
 	implements Transliterator
 {
+	
 
-	public String getLatinString(String arabicString) {
-		// TODO Auto-generated method stub
-		return null;
+	public static void main(String[] args) {
+		
+		String text = "اعطى الولد البنت كتابا ساءل امْرَأَتِي";
+		
+		Transliterator trans = new BuckwalterTransliterator();
+		
+		System.out.println(trans.getLatinString(text));
+	}
+
+	public  String getLatinString(String arabicString) {
+		
+		String normalizedText = TextNormalizer.normalizeText(arabicString);
+
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 0; i < normalizedText.length(); i++) {
+			
+			String unigram = arabicString.substring(i, i + 1);
+			
+				if ( BuckwalterTransliterationTable.unicodeMap.containsKey(unigram) )
+				{
+					
+					sb.append(BuckwalterTransliterationTable.unicodeMap.get(unigram));
+					
+				}else{
+					
+					sb.append(unigram);
+					
+				}
+		}
+		
+		
+		return sb.toString();
+		
 	}
 
 }
