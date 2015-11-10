21158a0d928c9771ed3b08afa36d43ea18f3dcd9
LTL <sam.hamed@gmail.com>
2015-11-10 20:50:20 +0100

    JUnit test

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/test/java/de/unidue/ltl/lugha/transliteration/BuckwalterTest.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.transliteration/src/test/java/de/unidue/ltl/lugha/transliteration/BuckwalterTest.java
@@ -1,5 +1,21 @@
 package de.unidue.ltl.lugha.transliteration;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class BuckwalterTest {
 
+	@Test
+	public void buckwalterTest() {
+		
+		Transliterator trans = new BuckwalterTransliterator();
+		
+		String text = "اعطى الولد البنت كتابا ساءل امْرَأَتِي";
+		
+		String latin = trans.getLatinString(text);
+		
+		assertEquals("AETY Alwld Albnt ktAbA sA'l Amora>atiy", latin);
+	}
+	
 }
