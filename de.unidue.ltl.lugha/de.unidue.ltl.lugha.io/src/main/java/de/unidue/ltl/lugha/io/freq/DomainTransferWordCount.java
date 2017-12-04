b249a382b1bd70e81fa9c8ee9af4e09da13cb661
Torsten Zesch <torsten.zesch@gmail.com>
2017-12-04 21:18:31 +0100

    a bit refactoring

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/main/java/de/unidue/ltl/lugha/io/freq/DomainTransferWordCount.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/main/java/de/unidue/ltl/lugha/io/freq/DomainTransferWordCount.java
@@ -1,15 +1,24 @@
 package de.unidue.ltl.lugha.io.freq;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
 import org.apache.uima.fit.pipeline.JCasIterable;
+import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.ConditionalFrequencyDistribution;
 import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.FrequencyDistribution;
+import de.tudarmstadt.ukp.dkpro.core.api.resources.DkproContext;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.tokit.RegexTokenizer;
 import de.unidue.ltl.lugha.normalization.DiacriticsRemover;
 import de.unidue.ltl.lugha.normalization.PunctuationRemover;
 import de.unidue.ltl.lugha.normalization.TextNormalizer;
@@ -18,137 +27,132 @@ import de.unidue.ltl.lugha.transliteration.Transliterator;
 
 
 public class DomainTransferWordCount {
-
-	public static void main(String[] args) throws Exception {
-
-		countsForDiacWords();
-		
-	}
 	
-	public static void countsForDiacWords() throws Exception {
+	private Transliterator buckwalter = new BuckwalterTransliterator();
 
-		/**
-		 * TODO:By TZ
-		 * Refer to the getCorpusPath(...) and change the pthes accordingly.
-		 */
+	public static void main(String[] args) 
+			throws Exception
+	{
+		DomainTransferWordCount exp = new DomainTransferWordCount();
+		exp.analyze();
+	}
 		
-		String corpusFile = CorpusFile.getCorpusPath(CorpusName.Tashkeela11Books);
+	public void analyze() 
+		throws Exception
+	{
+//		String corpusFile = CorpusFile.getCorpusPath(CorpusName.Tashkeela11Books);
 
-		CollectionReaderDescription reader = CollectionReaderFactory
-				.createReaderDescription(CorporaStatisitcsArabicReader.class,
-						CorporaStatisitcsArabicReader.PARAM_SENTENCES_FILE,
-						corpusFile,
+		String corpusFile = DkproContext.getContext().getWorkspace("corpora").getAbsolutePath() + "/arabic/tashkeela.txt";
+					
+	    ConditionalFrequencyDistribution<String,String> cfd = getCFD(
+	    		CollectionReaderFactory.createReaderDescription(
+						CorporaStatisitcsArabicReader.class,
+						CorporaStatisitcsArabicReader.PARAM_SENTENCES_FILE, corpusFile,
 						CorporaStatisitcsArabicReader.PARAM_ENCODING, "UTF-8",
-						CorporaStatisitcsArabicReader.HAS_DIACRITICS, "Yes");
-
-		
-		List<String> wordsList = getWords(reader);
-		
-		Transliterator buckwalter = new BuckwalterTransliterator();
-
-		
-		
-	    ConditionalFrequencyDistribution<String,String> cfd = new ConditionalFrequencyDistribution<String, String>();
-	    
-        for (String diacToken : wordsList) {
-
-        	//condition is w/o Diacritics
-        	        	
-				String condition = DiacriticsRemover.removeDiacritics(diacToken);
-				
-				String encode = buckwalter.getLatinString(condition);
-				
-				//Does diacToken has nunations?
-				if(buckwalter.getLatinString(diacToken).endsWith("F")){
-					
-					diacToken = DiacriticsRemover.removeTanweenFath(diacToken);
-					
-				}else if(buckwalter.getLatinString(diacToken).endsWith("N")){
-					
-					diacToken = DiacriticsRemover.removeTanweenDamm(diacToken);
-					
-				}else if(buckwalter.getLatinString(diacToken).endsWith("K")){
-					
-					diacToken = DiacriticsRemover.removeTanweenKasr(diacToken);
-					
-				}
-				
-				cfd.inc(encode, diacToken);
-				
-				/*
-				if(encode.equals("Elm")){
-//					if (!fullyDiacritized(diacToken)) {
-//						cfd.inc(encode, "partial");
-//					}
-//					else {						
-						cfd.inc(encode, diacToken);
-//					}
-				}
-				
-				if(encode.equals("*kr")){
-					cfd.inc(encode, diacToken);
-				}
-				
-				if(encode.equals("Eyn")){
-					cfd.inc(encode, diacToken);
-				}
-				 */
-				
-		}
+						CorporaStatisitcsArabicReader.HAS_DIACRITICS, "Yes"
+	    		),
+	    		AnalysisEngineFactory.createEngine(
+						RegexTokenizer.class,
+						RegexTokenizer.PARAM_TOKEN_BOUNDARY_REGEX, " "
+				)
+	    );
         
+        String[] roots = new String[]{"Elm", "*kr", "Eyn", "Zlm", "Eql", "$Er", "fjr", "nbE"};
         
-        System.out.println("==================================Elm==================================");
-        printSet(cfd, "Elm");
-
-        System.out.println("==================================Thkr=================================");
-        printSet(cfd, "*kr");
+        for (String root : roots) {
+        	printDivider(root);
+            printSet(cfd, root);
+        }
         
-        System.out.println("==================================Eyn==================================");
-        printSet(cfd, "Eyn");
+        System.out.println();
+        
+        // plot ambiguity
+        FrequencyDistribution<Integer> ambiguity = new FrequencyDistribution<Integer>();
+        for (String root : cfd.getConditions()) {
+        	ambiguity.inc(cfd.getFrequencyDistribution(root).getKeys().size());
+        }
+        List<Integer> levels = new ArrayList<Integer>(ambiguity.getKeys());
+        Collections.sort(levels);
+        for (Integer level : levels) {
+        	System.out.println(level + " : " + ambiguity.getCount(level));
+        }
+        
+        int[] sizes = new int[]{12,13,14,15,16,17,18,19,20};
+        for (int size : sizes) {
+        	printDistribution(cfd, size);
+        }
 
-        System.out.println("==================================Zlm==================================");
-        printSet(cfd, "Zlm");
-
-        System.out.println("==================================Eql==================================");
-        printSet(cfd, "Eql");
-
-        System.out.println("==================================$Er==================================");
-        printSet(cfd, "$Er");
-
-        System.out.println("==================================fjr==================================");
-        printSet(cfd, "fjr");
-
-        System.out.println("==================================nbE==================================");
-        printSet(cfd, "nbE");
-
+    }
+	
+	public DomainTransferWordCount() {
+		buckwalter = new BuckwalterTransliterator();
 	}
 	
-	private static void printSet(ConditionalFrequencyDistribution<String, String> cfd, String root) {
+	private ConditionalFrequencyDistribution<String, String> getCFD(CollectionReaderDescription reader, AnalysisEngine tokenizer)
+			throws AnalysisEngineProcessException
+	{
+	    ConditionalFrequencyDistribution<String,String> cfd = new ConditionalFrequencyDistribution<String, String>();
+
+	    int wordCount = 0;
+        for (JCas jcas : new JCasIterable(reader)) {
+        	tokenizer.process(jcas);
+        	
+        	for (Token token : JCasUtil.select(jcas, Token.class)) {
+				wordCount++;
+				
+				// TODO why do we need double normalization?
+	            String word = TextNormalizer.fullyNormalizeText(
+	            				PunctuationRemover.removePunctuation(
+	            						TextNormalizer.fullyNormalizeText(token.getCoveredText())));
+
+				// remove nunation
+				word = DiacriticsRemover.removeTanweenFath(word);
+				word = DiacriticsRemover.removeTanweenDamm(word);
+				word = DiacriticsRemover.removeTanweenKasr(word);
+
+				String root = DiacriticsRemover.removeDiacritics(word);
+			
+				String readableRoot = buckwalter.getLatinString(root);
+			
+				cfd.inc(readableRoot, word);
+			}
+        }
+        System.out.println("# words: " + wordCount);
+        System.out.println("# types: " + cfd.getConditions().size());
+
+        return cfd;
+	}
+	
+	private void printSet(ConditionalFrequencyDistribution<String, String> cfd, String root) {
         FrequencyDistribution<String> fd = cfd.getFrequencyDistribution(root);
         for(String key : fd.getKeys()){
 			System.out.println(key + " : " + fd.getCount(key));
 		}
 	}
 	
-	public static List<String>  getWords(CollectionReaderDescription reader) {
-		
-        List<String> words  = new ArrayList<String>();
-        
-        for (JCas jcas : new JCasIterable(reader)) {
-        	
-            String sentence = jcas.getDocumentText();
-            sentence = TextNormalizer.fullyNormalizeText(PunctuationRemover.removePunctuation(TextNormalizer.fullyNormalizeText(sentence)));
-            
-			for (String word : sentence.split(" ")) {
-				
-				words.add(word);
-				
-			}
-
+	private void printDivider(String root) {
+        System.out.println("====== "+ root + " ===================");
+	}
+	
+	private void printDistribution(ConditionalFrequencyDistribution<String, String> cfd, int sizeToPlot) {
+		System.out.println("Ambiguity size: " + sizeToPlot);
+		for (String root : cfd.getConditions()) {
+        	if (cfd.getFrequencyDistribution(root).getKeys().size() == sizeToPlot) {
+        		System.out.println(root + " (" + getDistributionString(cfd.getFrequencyDistribution(root)) + ")");
+        	}
         }
-        
-        return words;
-
+		System.out.println();
+	}
+	
+	private String getDistributionString(FrequencyDistribution<String> fd) {
+		List<Integer> values = new ArrayList<Integer>();
+		for (String key : fd.getKeys()) {
+			values.add(new Long(fd.getCount(key)).intValue());
+			// normalized
+//			values.add(new Double(Math.floor((double) fd.getCount(key)*1000 / fd.getN())).intValue());
+		}
+		Collections.sort(values);
+		return StringUtils.join(values, ",");
 	}
 
 }
