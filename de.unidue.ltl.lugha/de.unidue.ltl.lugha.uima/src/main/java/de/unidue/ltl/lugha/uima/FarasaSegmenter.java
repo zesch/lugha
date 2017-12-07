d5b49e09612a2028c6749713d07ab4632172f960
LTL <sam.hamed@gmail.com>
2017-12-07 15:08:13 +0100

    FarasaSegmenter and FarasaSegmenterTest

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.uima/src/main/java/de/unidue/ltl/lugha/uima/FarasaSegmenter.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.uima/src/main/java/de/unidue/ltl/lugha/uima/FarasaSegmenter.java
@@ -1,5 +1,37 @@
 package de.unidue.ltl.lugha.uima;
 
+import com.qcri.farasa.segmenter.Farasa;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FarasaSegmenter {
 
+	   public static void main(String[] args) throws IOException, FileNotFoundException, ClassNotFoundException {
+
+        
+        List<String> output = segment("النص المراد معالجته");
+        
+           for(String s: output){
+        	   System.out.println(s);
+           }
+
+     }
+	   
+	   public static List<String> segment(String line) throws IOException, FileNotFoundException, ClassNotFoundException {
+		   
+		   List<String> segmentedWords = new ArrayList<String> ();
+		   
+	        Farasa farasa = new Farasa();
+
+	        segmentedWords = farasa.segmentLine("النص المراد معالجته");
+
+	       
+		   return segmentedWords;
+	   }
+
+	
 }
--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.uima/src/test/java/de/unidue/ltl/lugha/uima/FarasaSegmenterTest.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.uima/src/test/java/de/unidue/ltl/lugha/uima/FarasaSegmenterTest.java
@@ -1,5 +1,24 @@
 package de.unidue.ltl.lugha.uima;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+
 public class FarasaSegmenterTest {
 
+	@Test
+	public void testFarasaSegmenter() throws Exception {
+		
+		List<String> output = FarasaSegmenter.segment("النص المراد معالجته");  
+		
+		assertEquals(3, output.size());
+		
+		assertEquals("ال+نص", output.get(0) );
+
+	}
+
+	
 }
