eedb28c5736f5c033296ee485e563dbbeef76bd5
LTL <sam.hamed@gmail.com>
2017-12-15 12:08:23 +0100

    add other paths

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/test/java/de/unidue/ltl/lugha/io/MergeCorporaFilesTest.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/test/java/de/unidue/ltl/lugha/io/MergeCorporaFilesTest.java
@@ -12,13 +12,14 @@ public class MergeCorporaFilesTest {
 //	public static final String targetFileUri = "src/test/resources/seq/seq.txt";
 //	public static final String sourceFolder = "src/test/resources/ar/rdim";
 
-	public static final String targetFileUri = "src/test/seq/seq.txt";
-	public static final String sourceFolder = "/Users/ltl2014lf/Downloads/main-paper/Khaleej-2004/economy";
+	public static final String targetFileUri = "src/test/seq/KACST.txt";
+	
+	public static final String sourceFolder = "/Users/ltl2014lf/Downloads/main-paper/Arabic-Newspapers";
 
 	@Test
 	public void testMergeCorporaFiles() throws Exception {
 		
-		MergeCorporaFiles ucf = new MergeCorporaFiles(targetFileUri, sourceFolder, "CP1256");//UTF-8
+		MergeCorporaFiles ucf = new MergeCorporaFiles(targetFileUri, sourceFolder, "UTF-8");//UTF-8, CP1256
         
 		//Merge corpora files
 //		ucf.constructNewFile(true, false);
