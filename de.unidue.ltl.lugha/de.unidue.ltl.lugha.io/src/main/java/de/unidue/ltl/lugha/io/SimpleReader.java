3a141916b1f1c0c39fe0a52c350e09c4fd3c3eb1
LTL <sam.hamed@gmail.com>
2018-01-10 16:50:15 +0100

    simple file reader

--- de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/main/java/de/unidue/ltl/lugha/io/SimpleReader.java
+++ de.unidue.ltl.lugha/de.unidue.ltl.lugha.io/src/main/java/de/unidue/ltl/lugha/io/SimpleReader.java
@@ -1,56 +1,17 @@
 package de.unidue.ltl.lugha.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.File;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class SimpleReader {
 
 	public static List<String> readFile(String fileName) throws Exception{
-
-		List<String> list = new ArrayList<String>();
 		
-		BufferedReader br = null;
-		FileReader fr = null;
-
-		try {
-
-			fr = new FileReader(fileName);
-			br = new BufferedReader(fr);
-
-			String  currentLine;
-
-			br = new BufferedReader(new FileReader(fileName));
-
-			while ((currentLine = br.readLine()) != null) {
-				list.add(currentLine);
-			}
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
-		} finally {
-
-			try {
-
-				if (br != null)
-					br.close();
-
-				if (fr != null)
-					fr.close();
-
-			} catch (IOException ex) {
-
-				ex.printStackTrace();
-
-			}
-
-		}
-		
+		List<String> list = FileUtils.readLines(new File(fileName));
+				
 		return list;
-}
+	}
 	
 }
